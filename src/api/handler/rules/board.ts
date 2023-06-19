d9deb450ff86cc76fcc58431a92a11b7312f5291
soramar <fujitanisora0414@i.softbank.jp>
2023-06-19 12:41:36 +0900

    add: board validatio実装

---
+++ src/api/handler/rules/board.ts
@@ -0,0 +1,33 @@
+import { check, body } from "express-validator";
+
+export const boardCreateRule = [
+  check("title")
+    .not()
+    .isEmpty()
+    .withMessage("title is required")
+    .isLength({ max: 255 })
+    .withMessage("title mast be at largest 255 characters"),
+  check("content")
+    .isLength({ max: 255 })
+    .withMessage("content mast be at largest 255 characters"),
+  check("boardImage")
+    .isLength({ max: 255 })
+    .withMessage("boardImage mast be at largest 255 characters"),
+  // todo: 正規表現バリデーションの実装
+];
+
+export const boardUpdateRule = [
+  check("title")
+    .not()
+    .isEmpty()
+    .withMessage("title is required")
+    .isLength({ max: 255 })
+    .withMessage("title mast be at largest 255 characters"),
+  check("content")
+    .isLength({ max: 255 })
+    .withMessage("content mast be at largest 255 characters"),
+  check("boardImage")
+    .isLength({ max: 255 })
+    .withMessage("boardImage mast be at largest 255 characters"),
+  // todo: 正規表現バリデーションの実装
+];
