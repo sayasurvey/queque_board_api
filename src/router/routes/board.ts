54adb21d23d8176086de9b6cdb0f5bdb70eeb2c3
soramar <fujitanisora0414@i.softbank.jp>
2023-06-22 15:22:31 +0900

    fix: showBoardのrouter修正

--- src/router/routes/board.ts
+++ src/router/routes/board.ts
@@ -6,7 +6,7 @@ const boardContext = new BoardController();
 
 router.get("/boards", boardContext.allBoard);
 router.post("/board", boardContext.postBoard);
-router.get("board/:id", boardContext.showBoard);
+router.get("/board/:id", boardContext.showBoard);
 router.put("/board/:id", boardContext.putBoard);
 router.delete("/board/:id", boardContext.deleteBoard);
 
