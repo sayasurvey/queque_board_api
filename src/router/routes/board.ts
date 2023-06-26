d9a9af626931f7706c8a9d1bf71120a7fe8dc5d8
soramar <fujitanisora0414@i.softbank.jp>
2023-06-26 16:13:09 +0900

    add: validationのmiddleware適用

--- src/router/routes/board.ts
+++ src/router/routes/board.ts
@@ -1,13 +1,18 @@
 import express from "express";
 const router = express.Router();
 const { BoardController } = require("../../api/controller/BoardController");
+const {
+  boardCreateRule,
+  boardUpdateRule,
+} = require("../../api/handler/rules/board.ts");
+const { validateError } = require("../../api/handler/rules/validateError");
 
 const boardContext = new BoardController();
 
 router.get("/boards", boardContext.allBoard);
-router.post("/board", boardContext.postBoard);
+router.post("/board", boardCreateRule, validateError, boardContext.postBoard);
 router.get("/board/:id", boardContext.showBoard);
-router.put("/board/:id", boardContext.putBoard);
+router.put("/board/:id", boardUpdateRule, validateError, boardContext.putBoard);
 router.delete("/board/:id", boardContext.deleteBoard);
 
 module.exports = router;
