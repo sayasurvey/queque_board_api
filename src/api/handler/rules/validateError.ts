a9f387a7c3493f92d8204dac7651882adf7de030
soramar <fujitanisora0414@i.softbank.jp>
2023-06-26 22:06:55 +0900

    fix: validationErrorのstatusCode変更

--- src/api/handler/rules/validateError.ts
+++ src/api/handler/rules/validateError.ts
@@ -8,7 +8,7 @@ export const validateError = (
 ): any => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
+    return res.status(442).json({ errors: errors.array() });
   }
   next();
 };
