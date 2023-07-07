ce14b08f8875efdb70eb20e68a30690e6da9ea4b
soramar <fujitanisora0414@i.softbank.jp>
2023-07-07 16:34:16 +0900

    fix: UserControllerのエラーメッセージ修正

--- src/api/controller/UserController.ts
+++ src/api/controller/UserController.ts
@@ -1,29 +1,38 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { getUsers, updateUser, getUser, destroyUser } from "../model/User";
+import {
+  errorHandler,
+  BadRequestError,
+  NotFoundError,
+} from "../handler/exception/customError";
 
 export class UserController {
   async allUser(_req: Request, res: Response): Promise<void> {
     const users = await getUsers();
     res.status(200).json({
+      message: "user get all success",
       users,
     });
   }
 
-  async showUser(req: Request, res: Response): Promise<void> {
+  async showUser(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
     const id = parseInt(req.params.id);
     try {
       const user = await getUser(id);
+
       if (!user) {
-        throw new Error("this user does not exist");
+        throw new NotFoundError(404, "this users does not get", "info");
       }
       res.status(200).json({
         message: "show user",
         user,
       });
     } catch (error: any) {
-      res.status(401).json({
-        message: error.message,
-      });
+      return next(errorHandler(error, res));
     }
   }
 
@@ -33,10 +42,10 @@ export class UserController {
       const id = parseInt(req.params.id);
       const user = await updateUser(id, name, email, iconImage);
       if (!user) {
-        throw new Error("this user does not exist");
+        throw new Error("this user does not update");
       }
       res.status(201).json({
-        message: "user update success",
+        message: "this user update is success",
         user,
       });
     } catch (error: any) {
@@ -51,10 +60,10 @@ export class UserController {
       const id = parseInt(req.params.id);
       const user = await destroyUser(id);
       if (!user) {
-        throw new Error("this user does not exist");
+        throw new Error("this user does not delete");
       }
       res.status(201).json({
-        message: "user delete success",
+        message: "this user delete is success",
       });
     } catch (error: any) {
       res.status(401).json({
