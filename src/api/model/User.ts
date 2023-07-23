8634bbdfc3122ae6525089c7f6e5bc52633196ae
soramar <fujitanisora0414@i.softbank.jp>
2023-07-23 17:01:38 +0900

    fix: UserModel内prismaの例外処理

--- src/api/model/User.ts
+++ src/api/model/User.ts
@@ -1,20 +1,35 @@
 import { User } from "@prisma/client";
 import { prismaContext } from "../../lib/prismaContext";
+import { CustomException } from "../handler/exception/customError";
 
 export const getUsers = async () => {
-  const users = await prismaContext.user.findMany();
+  const users = await prismaContext.user.findMany().catch(() => {
+    throw new CustomException(
+      400,
+      "this user does not get all caused prisma error",
+      "info"
+    );
+  });
   return users;
 };
 
 export const getUser = async (id: number): Promise<User | null> => {
-  const user = await prismaContext.user.findUnique({
-    where: {
-      id,
-    },
-    include: {
-      boards: true,
-    },
-  });
+  const user = await prismaContext.user
+    .findUnique({
+      where: {
+        id,
+      },
+      include: {
+        boards: true,
+      },
+    })
+    .catch(() => {
+      throw new CustomException(
+        400,
+        "this user does not get caused prisma error",
+        "info"
+      );
+    });
 
   return user;
 };
@@ -25,18 +40,34 @@ export const updateUser = (
   email: string,
   iconImage: string
 ): Promise<User> => {
-  const user = prismaContext.user.update({
-    where: { id },
-    data: { name, email, iconImage },
-  });
+  const user = prismaContext.user
+    .update({
+      where: { id },
+      data: { name, email, iconImage },
+    })
+    .catch(() => {
+      throw new CustomException(
+        400,
+        "this user does not update caused prisma error",
+        "info"
+      );
+    });
 
   return user;
 };
 
 export const destroyUser = (id: number): Promise<User> => {
-  const user = prismaContext.user.delete({
-    where: { id },
-  });
+  const user = prismaContext.user
+    .delete({
+      where: { id },
+    })
+    .catch(() => {
+      throw new CustomException(
+        400,
+        "this user does not delete caused prisma error",
+        "info"
+      );
+    });
 
   return user;
 };
