a86f7dddfbf68f542a55efeb5efab4e9ad673a5d
ユーザ名 <sayasurvey@yahoo.co.jp>
2023-07-26 01:27:56 +0900

    FIX: リクエストのトークンからデコード処理とエラー分岐

--- src/middleware/authenticateUser.ts
+++ src/middleware/authenticateUser.ts
@@ -8,12 +8,21 @@ const jwtSecret = process.env.JWT_SECRET_KEY || "";
 // JWTのデコードとユーザー確認を行うミドルウェア
 const authenticateUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const token = "";
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+      return res.status(401).json({ message: 'Authentication is required' });
+    }
+
     const decode = await tokenDecode(token);
+    
+    if (!decode) {
+      return res.status(401).json({ message: 'The token is invalid' });
+    }
 
   } catch (error) {
-    // デフォルトのエラークラスでthrow
-    throw new Error("Logout failed");
+    return res.status(500).json({ message: 'The token verification failed' });
   }
   next();
 };
