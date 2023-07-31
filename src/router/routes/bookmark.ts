594ff0dd0b6e91fe38df423c4309c97cbbc54f51
fujitani.s <fujitani.s@impl.co.jp>
2023-08-01 08:46:49 +0900

    fix: tokenVerify実装

--- src/router/routes/bookmark.ts
+++ src/router/routes/bookmark.ts
@@ -1,13 +1,15 @@
 import express from "express";
 const router = express.Router();
 import { BookmarkController } from "../../api/controller/BookMarkController";
+import tokenVerify from "../../middleware/tokenVerify";
 
 const bookmarkContext = new BookmarkController();
 
-router.get("/user/:user_id/bookmark", bookmarkContext.getUserBookmark);
-router.post("/user/:user_id/bookmark", bookmarkContext.postBookmark);
+router.get("/user/:user_id/bookmark", tokenVerify, bookmarkContext.getUserBookmark);
+router.post("/user/:user_id/bookmark", tokenVerify, bookmarkContext.postBookmark);
 router.delete(
   "/user/:user_id/bookmark/:bookmark_id",
+  tokenVerify,
   bookmarkContext.deleteBookmark
 );
 
