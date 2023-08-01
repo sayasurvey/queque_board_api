b76fba89d567c8fbdbff4f61c5d21713b92d8d48
sayasurvey <sayasurvey@yahoo.co.jp>
2023-08-02 00:35:27 +0900

    FIX: 一覧のURL変更

--- src/router/routes/bookmark.ts
+++ src/router/routes/bookmark.ts
@@ -5,7 +5,7 @@ import tokenVerify from "../../middleware/tokenVerify";
 
 const bookmarkContext = new BookmarkController();
 
-router.get("/user/:user_id/bookmark", tokenVerify, bookmarkContext.getUserBookmark);
+router.get("/user/:user_id/bookmarks", tokenVerify, bookmarkContext.getUserBookmark);
 router.post("/user/:user_id/bookmark", tokenVerify, bookmarkContext.postBookmark);
 router.delete(
   "/user/:user_id/bookmark/:bookmark_id",
